01a1b5e76b45117f9ce2f0538695c590f8de0bb9
Igor Lamas <56036312+lamas250@users.noreply.github.com>
2023-02-10 15:12:20 -0300

    feat: context tag

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+
 import { ThemeProvider } from '@mui/material';
 import { TagProvider } from './context/tag';
 
