a5d1f7dcd016135a16739ee59a25bb8cf362d2cd
Igor Lamas <56036312+lamas250@users.noreply.github.com>
2023-02-14 14:05:23 -0300

    new url

--- src/infra/http/axios-http-client.ts
+++ src/infra/http/axios-http-client.ts
@@ -1,8 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
-export const axiosClient =  axios.create({
-  baseURL: "https://2ip08dplgl.execute-api.us-east-1.amazonaws.com/prod/v1",
+export const axiosClient = axios.create({
+  baseURL: 'https://mykxq9t7yi.execute-api.us-east-1.amazonaws.com/prod/v1',
   headers: {
-    "Content-type": "application/json"
-  }
+    'Content-type': 'application/json',
+  },
 });
